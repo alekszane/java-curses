35187c824b2e88edb7617a4c8bf6c10844820db7
alekszane <alekszane@gmail.com>
2017-09-26 01:27:34 +0300

    user input

---
+++ Calculator.java
@@ -0,0 +1,31 @@
+/*
+* Класс реализует калькулятор
+* */
+
+public class Calculator{
+    /*результат вычисления*/
+
+    private int result;
+
+    /*Суммируем аргуенты
+    @param  и params аргументы суммирования*/
+
+    public void add (int ... params){
+        for (Integer param : params){
+            this.result += param;
+        }
+    }
+
+    /*Получить результат
+    @return результат вычисления*/
+
+    public int getResult(){
+        return this.result;
+    }
+    /*Очистить результат вычислений*/
+
+    public void cleanResult(){
+        this.result = 0;
+    }
+
+}
